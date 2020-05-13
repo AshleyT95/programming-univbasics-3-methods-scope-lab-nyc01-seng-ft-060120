997fd7eb5a167750e1dbfe04f4f8f682ebcf5324
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-05-13 21:58:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,6 +5,19 @@ def mario
 end
 
 def toadstool
-  puts mario
+  status= 'Thank You Mario, But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase= "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
+
+
+
